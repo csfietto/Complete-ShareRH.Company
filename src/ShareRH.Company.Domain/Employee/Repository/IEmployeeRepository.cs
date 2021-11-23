f5f0aacf2ae5933feb8a44d5c2ff4acbd0c2d322
cristiano.fietto <cristiano.fietto@capgemini.com>
2021-11-22 23:09:49 -0300

    Add project files.

---
+++ src/ShareRH.Company.Domain/Employee/Repository/IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using ShareRH.Company.Domain.Employee.Models;
+
+namespace ShareRH.Company.Domain.Employee.Repository
+{
+    /// <summary>
+    /// A repository class for <see cref="IEmployee"/>s.
+    /// </summary>
+    public interface IEmployeeRepository
+    {
+        /// <summary>
+        /// Add into the repository an instance of <see cref="IEmployee"/>.
+        /// </summary>
+        void Add(IEmployee employee);
+
+        /// <summary>
+        /// Get an instance of <see cref="IEmployee"/> from the repository.
+        /// </summary>
+        /// <param name="id">The <see cref="IEmployee"/> identifier to select in the repository.</param>
+        /// <returns>An instance of <see cref="IEmployeeRepository"/> that matched with the given identifier, otherwise a null instance.</returns>
+        IEmployee Get(string id);
+    }
+}
---
+++ src/ShareRH.Company.Domain/Employee/Service/IEmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using ShareRH.Company.Domain.Employee.Enums;
+using ShareRH.Company.Domain.Employee.Models;
+using ShareRH.Company.Domain.Responses;
+
+namespace ShareRH.Company.Domain.Employee.Service
+{
+    /// <summary>
+    /// Encapsulate the functions to manipulate the <see cref="IEmployee"/> instances.
+    /// </summary>
+    public interface IEmployeeService
+    {
+        /// <summary>
+        /// Creates a new employee and add in the company base list.
+        /// </summary>
+        /// <returns>An instance of <see cref="Response"/> with the result of the operation.</returns>
+        public EmployeeResponse Hire(Function function, string name, ContractTypes type, int workHoursPerDay);
+
+        /// <summary>
+        /// Get an instance of <see cref="IEmployee"/> from the repository based on the given identifier.
+        /// A null value will be returned in case of non-existent value.
+        /// </summary>
+        /// <param name="id">The identifier to be selected.</param>
+        /// <returns>An instance of <see cref="IEmployee"/> that matched with the identifier.</returns>
+        public EmployeeResponse Get(string id);
+    }
+}
